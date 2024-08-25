9a394273cc83fa4378155389e2339e642b88d641
devanshjain1712 <devanshjain1712@github.com>
2024-08-25 19:39:47 +0530

    ASD

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors') 
+const app = express();
+
+app.use(cors());
+
+app.put('/',(req,res)=>{
+    const body = req.body;
+    console.log({body})
+    res.send({success:true})
+})
+app.listen(5000,()=>{
+    console.log("server started at port 5000")
+});
